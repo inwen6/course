0b329e82a2193ffdf0a1dd29241cd88d973f461e
inwen6 <4462971@qq.com>
2021-03-03 14:27:00 +0800

    menu 修改

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,8 @@
     <header id="header">
       <section class="container">
         <h1 id="logo">
-          <a href="#" title="谷粒学院">
-            <img src="~/assets/img/logo.png" width="100%" alt="谷粒学院">
+          <a href="#" title="xx学院">
+            <img src="~/assets/img/logo.png" width="100%" alt="xx学院">
           </a>
         </h1>
         <div class="h-r-nsl">
@@ -13,54 +13,64 @@
             <router-link to="/" tag="li" active-class="current" exact>
               <a>首页</a>
             </router-link>
-            <router-link to="/course" tag="li" active-class="current">
-              <a>课程</a>
+            <router-link to="/course" tag="li" active-class="current linkStyle">
+              <a>教学</a>
+
+              <div class="positionsDiv"> 
+                <router-link tag="div" to="/course">课程</router-link>
+                <router-link tag="div" to="/">作业</router-link>
+              </div>
+
             </router-link>
-            <router-link to="/teacher" tag="li" active-class="current">
-              <a>名师</a>
+            <router-link to="/teacher" tag="li" active-class="current linkStyle">
+              <a>学工</a>
+
+              <div class="positionsDiv"> 
+                <router-link tag="div" to="/course">满意度</router-link>
+                <router-link tag="div" to="/">考勤</router-link>
+                <router-link tag="div" to="/course">演讲</router-link>
+                <router-link tag="div" to="/">活动</router-link>
+              </div>
+
             </router-link>
             <router-link to="/article" tag="li" active-class="current">
-              <a>文章</a>
-            </router-link>
-            <router-link to="/qa" tag="li" active-class="current">
-              <a>问答</a>
-            </router-link>
+              <a>就业</a>
+
+              <div class="positionsDiv"> 
+                <router-link tag="div" to="/course">资讯</router-link> 
+                <router-link tag="div" to="/">活动</router-link>
+                <router-link tag="div" to="/">案例</router-link>
+              </div>
+
+            </router-link> 
+          </ul> 
+          <ul class="h-r-login">
+            <li v-if="!loginInfo.id" id="no-login">
+              <a href="/login" title="登录">
+                <em class="icon18 login-icon">&nbsp;</em>
+                <span class="vam ml5">登录</span>
+              </a>
+              <!-- |
+              <a href="/register" title="注册">
+                <span class="vam ml5">注册</span>
+              </a> -->
+            </li>
+            <li v-if="loginInfo.id" id="is-login-one" class="mr10">
+              <a id="headerMsgCountId" href="#" title="消息">
+                <em class="icon18 news-icon">&nbsp;</em>
+              </a>
+              <q class="red-point" style="display: none">&nbsp;</q>
+            </li>
+            <li v-if="loginInfo.id" id="is-login-two" class="h-r-user">
+              <a href="/ucenter" title>
+                <img :src="loginInfo.avatar" width="30" height="30" class="vam picImg" alt>
+                <span id="userName" class="vam disIb">{{ loginInfo.nickname }}</span>
+              </a>
+              <a href="javascript:void(0);" title="退出" @click="logout()" class="ml5">退出</a>
+            </li>
+            <!-- /未登录显示第1 li；登录后显示第2，3 li -->
           </ul>
-          <!-- / nav -->
-           <!-- / nav -->
-<ul class="h-r-login">
-    <li v-if="!loginInfo.id" id="no-login">
-        <a href="/login" title="登录">
-            <em class="icon18 login-icon">&nbsp;</em>
-            <span class="vam ml5">登录</span>
-        </a>
-        |
-        <a href="/register" title="注册">
-            <span class="vam ml5">注册</span>
-        </a>
-    </li>
-    <li v-if="loginInfo.id" id="is-login-one" class="mr10">
-        <a id="headerMsgCountId" href="#" title="消息">
-            <em class="icon18 news-icon">&nbsp;</em>
-        </a>
-        <q class="red-point" style="display: none">&nbsp;</q>
-    </li>
-    <li v-if="loginInfo.id" id="is-login-two" class="h-r-user">
-        <a href="/ucenter" title>
-            <img
-                 :src="loginInfo.avatar"
-                 width="30"
-                 height="30"
-                 class="vam picImg"
-                 alt
-                 >
-            <span id="userName" class="vam disIb">{{ loginInfo.nickname }}</span>
-        </a>
-        <a href="javascript:void(0);" title="退出" @click="logout()" class="ml5">退出</a>
-    </li>
-    <!-- /未登录显示第1 li；登录后显示第2，3 li -->
-</ul>
-          <aside class="h-r-search">
+          <!-- <aside class="h-r-search">
             <form action="#" method="post">
               <label class="h-r-s-box">
                 <input type="text" placeholder="输入你想学的课程" name="queryCourse.courseName" value>
@@ -69,7 +79,7 @@
                 </button>
               </label>
             </form>
-          </aside>
+          </aside> -->
         </div>
         <aside class="mw-nav-btn">
           <div class="mw-nav-icon"></div>
@@ -78,8 +88,8 @@
       </section>
     </header>
     <!-- /公共头引入 -->
-      
-    <nuxt/>
+
+    <nuxt />
 
     <!-- 公共底引入 -->
     <footer id="footer">
@@ -131,25 +141,25 @@
   </div>
 </template>
 <script>
-import '~/assets/css/reset.css'
-import '~/assets/css/theme.css'
-import '~/assets/css/global.css'
-import '~/assets/css/web.css'
-import '~/assets/css/base.css'
-import '~/assets/css/activity_tab.css'
-import '~/assets/css/bottom_rec.css'
-import '~/assets/css/nice_select.css'
-import '~/assets/css/order.css'
-import '~/assets/css/swiper-3.3.1.min.css'
-import "~/assets/css/pages-weixinpay.css"
+  import '~/assets/css/reset.css'
+  import '~/assets/css/theme.css'
+  import '~/assets/css/global.css'
+  import '~/assets/css/web.css'
+  import '~/assets/css/base.css'
+  import '~/assets/css/activity_tab.css'
+  import '~/assets/css/bottom_rec.css'
+  import '~/assets/css/nice_select.css'
+  import '~/assets/css/order.css'
+  import '~/assets/css/swiper-3.3.1.min.css'
+  import "~/assets/css/pages-weixinpay.css"
 
-import cookie from 'js-cookie'
-import loginApi from '@/api/login'
+  import cookie from 'js-cookie'
+  import loginApi from '@/api/login'
 
-export default {
-  data() {
-    return {
-        token:'',
+  export default {
+    data() {
+      return {
+        token: '',
         loginInfo: {
           id: '',
           age: '',
@@ -158,53 +168,91 @@ export default {
           nickname: '',
           sex: ''
         }
-    }
-  },
-  created() {
-    //获取路径里面token值
-    this.token = this.$route.query.token
-    console.log(this.token)
-    if(this.token) {//判断路径是否有token值
-       this.wxLogin()
-    }
-
-    this.showInfo()
-  },
-  methods:{
-    //微信登录显示的方法
-    wxLogin() {
-      //console.log('************'+this.token)
-      //把token值放到cookie里面
-      cookie.set('guli_token',this.token,{domain: 'localhost'})
-      cookie.set('guli_ucenter','',{domain: 'localhost'})
-     //console.log('====='+cookie.get('guli_token'))
-      //调用接口，根据token值获取用户信息
-      loginApi.getMemberInfo()
-        .then(response => {
-          // console.log('################'+response.data.data.userInfo)
-           this.loginInfo = response.data.data.member
-           cookie.set('guli_ucenter',this.loginInfo,{domain: 'localhost'})
-        })
-    },
-    //创建方法，从cookie获取用户信息
-    showInfo() {
-      //从cookie获取用户信息
-      var userStr = cookie.get('guli_ucenter')
-      // 把字符串转换json对象(js对象)
-      if(userStr) {
-        this.loginInfo = JSON.parse(userStr)
       }
     },
+    created() {
+      //获取路径里面token值
+      this.token = this.$route.query.token
+      console.log(this.token)
+      if (this.token) { //判断路径是否有token值
+        this.wxLogin()
+      }
 
-    //退出
-    logout() {
-      //清空cookie值
-      cookie.set('guli_token','',{domain: 'localhost'})
-      cookie.set('guli_ucenter','',{domain: 'localhost'})
-      //回到首页面
-      window.location.href = "/";
+      this.showInfo()
+    },
+    methods: {
+      //微信登录显示的方法
+      wxLogin() {
+        //console.log('************'+this.token)
+        //把token值放到cookie里面
+        cookie.set('guli_token', this.token, {
+          domain: 'localhost'
+        })
+        cookie.set('guli_ucenter', '', {
+          domain: 'localhost'
+        })
+        //console.log('====='+cookie.get('guli_token'))
+        //调用接口，根据token值获取用户信息
+        loginApi.getMemberInfo()
+          .then(response => {
+            // console.log('################'+response.data.data.userInfo)
+            this.loginInfo = response.data.data.member
+            cookie.set('guli_ucenter', this.loginInfo, {
+              domain: 'localhost'
+            })
+          })
+      },
+      //创建方法，从cookie获取用户信息
+      showInfo() {
+        //从cookie获取用户信息
+        var userStr = cookie.get('guli_ucenter')
+        // 把字符串转换json对象(js对象)
+        if (userStr) {
+          this.loginInfo = JSON.parse(userStr)
+        }
+      },
+
+      //退出
+      logout() {
+        //清空cookie值
+        cookie.set('guli_token', '', {
+          domain: 'localhost'
+        })
+        cookie.set('guli_ucenter', '', {
+          domain: 'localhost'
+        })
+        //回到首页面
+        window.location.href = "/";
+      }
+
     }
+  };
 
-  }
-};
 </script>
+<style scoped>
+  .nav li{
+    position: relative;
+  }
+  .nav li:hover .positionsDiv{
+    display: block;
+  } 
+  .positionsDiv{ 
+    width: 80px; 
+    background: #eee;
+    position: absolute;
+    top: 52px;
+    z-index: 9999;
+    display: none;
+    box-sizing: border-box; 
+  }
+  .positionsDiv div{
+    width: 100%;
+    height: 40px; 
+    text-align: center;
+    line-height: 40px;
+    cursor: pointer;
+  }
+  .positionsDiv div:hover{
+    background-color: darkcyan;
+  }
+</style>
