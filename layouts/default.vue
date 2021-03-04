33c71d818bbd98b2aa8f4dbc8c44fc92f7d30d2b
inwen6 <4462971@qq.com>
2021-03-04 13:52:00 +0800

    合并冲突

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,7 +22,6 @@
               </div>
 
             </router-link>
-<<<<<<< HEAD
             <router-link to="/teacher" tag="li" active-class="current linkStyle">
               <a>学工</a>
 
@@ -72,65 +71,6 @@
             <!-- /未登录显示第1 li；登录后显示第2，3 li -->
           </ul>
           <!-- <aside class="h-r-search">
-=======
-           <!-- <router-link to="/teacher" tag="li" active-class="current">
-              <a>名师</a>
-            </router-link>
-            <router-link to="/article" tag="li" active-class="current">
-              <a>文章</a>
-            </router-link> -->
-            <router-link to="/activity" tag="li" active-class="current">
-              <a>活动</a>
-            </router-link>
-            <router-link to="/case" tag="li" active-class="current">
-              <a>就业案例</a>
-            </router-link>
-            <router-link to="/jobs" tag="li" active-class="current">
-              <a>招聘</a>
-            </router-link>
-            <router-link to="/task" tag="li" active-class="current">
-              <a>作业</a>
-            </router-link>
-        <!--    <router-link to="/qa" tag="li" active-class="current">
-              <a>问答</a>
-            </router-link> -->
-          </ul>
-          <!-- / nav -->
-           <!-- / nav -->
-<ul class="h-r-login">
-    <li v-if="!loginInfo.avatar" id="no-login">
-        <a href="/login" title="登录">
-            <em class="icon18 login-icon">&nbsp;</em>
-            <span class="vam ml5">登录</span>
-        </a>
-        |
-        <a href="/register" title="注册">
-            <span class="vam ml5">注册</span>
-        </a>
-    </li>
-    <li v-if="loginInfo.avatar" id="is-login-one" class="mr10">
-        <a id="headerMsgCountId" href="#" title="消息">
-            <em class="icon18 news-icon">&nbsp;</em>
-        </a>
-        <q class="red-point" style="display: none">&nbsp;</q>
-    </li>
-    <li v-if="loginInfo.avatar" id="is-login-two" class="h-r-user">
-        <a href="/ucenter" title>
-            <img
-                 :src="loginInfo.avatar"
-                 width="30"
-                 height="30"
-                 class="vam picImg"
-                 alt
-                 >
-            <span id="userName" class="vam disIb">{{ loginInfo.name }}</span>
-        </a>
-        <a href="javascript:void(0);" title="退出" @click="logout()" class="ml5">退出</a>
-    </li>
-    <!-- /未登录显示第1 li；登录后显示第2，3 li -->
-</ul>
-          <aside class="h-r-search">
->>>>>>> 87b82375813f7b3ac68ec1c13d0f4bb04a631675
             <form action="#" method="post">
               <label class="h-r-s-box">
                 <input type="text" placeholder="输入你想学的课程" name="queryCourse.courseName" value>
@@ -149,11 +89,7 @@
     </header>
     <!-- /公共头引入 -->
 
-<<<<<<< HEAD
     <nuxt />
-=======
-    <nuxt/>
->>>>>>> 87b82375813f7b3ac68ec1c13d0f4bb04a631675
 
     <!-- 公共底引入 -->
     <footer id="footer">
@@ -234,23 +170,12 @@
         }
       }
     },
-<<<<<<< HEAD
     created() {
       //获取路径里面token值
       this.token = this.$route.query.token
       console.log(this.token)
       if (this.token) { //判断路径是否有token值
         this.wxLogin()
-=======
-    //创建方法，从cookie获取用户信息
-    showInfo() {
-      //从cookie获取用户信息
-      var userStr = cookie.get('guli_ucenter')
-      // 把字符串转换json对象(js对象)
-      if(userStr) {
-        this.loginInfo = JSON.parse(userStr)
-        console.log(this.loginInfo)
->>>>>>> 87b82375813f7b3ac68ec1c13d0f4bb04a631675
       }
 
       this.showInfo()
@@ -300,16 +225,6 @@
         window.location.href = "/";
       }
 
-<<<<<<< HEAD
-=======
-    //退出
-    logout() {
-      //清空cookie值
-      cookie.set('Authorization','',{domain: 'localhost'})
-      cookie.set('guli_ucenter','',{domain: 'localhost'})
-      //回到首页面
-      window.location.href = "/";
->>>>>>> 87b82375813f7b3ac68ec1c13d0f4bb04a631675
     }
   };
 
@@ -340,9 +255,4 @@
   .positionsDiv div:hover{
     background-color: darkcyan;
   }
-<<<<<<< HEAD
 </style>
-=======
-};
-</script>
->>>>>>> 87b82375813f7b3ac68ec1c13d0f4bb04a631675
